6c3b6ee14e5ec7d2e1ad8442c1fbf36aa72af5ea
Ricardo Paiva <ricardoccpaiva@gmail.com>
2016-01-03 23:05:37 +0000

    Added migration files

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,11 @@
 class PagesController < ApplicationController
 
   def home
-
+    article = Article.new(title: "Titalu 1", description: "desc 1")
+    article.save
   end
 
   def about
-
+  
   end
 end
