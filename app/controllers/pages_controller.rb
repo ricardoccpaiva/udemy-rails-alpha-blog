2f24c14e5fd42de3c912843ccab79172e10d9cf9
Ricardo Paiva <ricardoccpaiva@gmail.com>
2016-01-07 23:08:52 +0000

    created articles model with validation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,17 @@
 class PagesController < ApplicationController
 
+  
   def home
     article = Article.new(title: "Titalu 1", description: "desc 1")
     article.save
   end
 
   def about
-  
+    article = Article.find(1)
+    article.description = "new desc 1"
+    article.save
+
+    article = Article.find(2)
+    article.destroy
   end
 end
