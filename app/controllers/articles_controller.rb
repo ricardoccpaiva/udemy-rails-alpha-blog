16814500e506fad1bf664135ae71c057142da157
Ricardo Paiva <ricardoccpaiva@gmail.com>
2016-02-09 14:37:51 +0000

    minor changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,7 +26,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-
+    
     if @article.save
       flash[:notice] = "Article was created"
       redirect_to article_path(@article)
