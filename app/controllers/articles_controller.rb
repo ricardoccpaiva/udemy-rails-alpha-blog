8c058a1e07d09e7d8aaf3c20cb44f3a23d142b3f
Ricardo Paiva <ricardoccpaiva@gmail.com>
2016-02-14 23:34:01 +0000

    added bootstrap css style to some pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -29,7 +29,7 @@ class ArticlesController < ApplicationController
     
     if @article.save
       flash[:notice] = "Article was created"
-      redirect_to article_path(@article)
+      redirect_to articles_path
     else
       render 'new'
     end
