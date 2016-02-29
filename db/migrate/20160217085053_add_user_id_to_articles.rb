b84b1d8ce795394de714993a29111354e6e74ca1
Ricardo Paiva <ricardoccpaiva@gmail.com>
2016-02-29 22:57:46 +0000

    removed foreign key

--- db/migrate/20160217085053_add_user_id_to_articles.rb
+++ db/migrate/20160217085053_add_user_id_to_articles.rb
@@ -1,6 +1,5 @@
 class AddUserIdToArticles < ActiveRecord::Migration
   def change
     add_column :articles, :user_id, :integer
-    add_reference :articles, :users, index: true, foreign_key: true
   end
 end
